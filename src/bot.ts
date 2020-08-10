b54f30ff469ac467f2da6f42d92a8acfd3a7e806
Hossein Yousefi <hossein@identi.app>
2020-08-10 22:23:12 +0200

    stay tuned

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import Telegraf, { Markup } from "telegraf";
 import axios from "axios";
-const LocalSession = require('telegraf-session-local')
+const LocalSession = require("telegraf-session-local");
 
 const { NODE_ENV } = process.env;
 
@@ -15,18 +15,17 @@ if (!TELEGRAM_TOKEN) {
 const bot = new Telegraf(TELEGRAM_TOKEN);
 const localSession = new LocalSession();
 
-axios.defaults.baseURL = 'https://app.clickup.com/api/v2';
+axios.defaults.baseURL = "https://app.clickup.com/api/v2";
 
 bot.use(localSession.middleware());
 
 bot.start((ctx) => {
   const { message } = ctx;
-  ctx['session'].code = message.text.substr(6);
-  ctx.replyWithMarkdown(ctx['session']);
+  const code = ctx["session"].code = message.text.substr(6);
   return ctx.replyWithMarkdown(
-    `*Welcome to The Unofficial Clickup Bot for Telegram*\n\n _Issues and suggestions: https://github.com/HosseinYousefi/clkupbot/issues_\n`,
+    `*Welcome to The Unofficial Clickup Bot for Telegram* - Stay tuned!\n\n_Contributions are welcome: https://github.com/HosseinYousefi/clkupbot/_\n`,
     {
-      'disable_web_page_preview': true
+      "disable_web_page_preview": true
     }
   );
 });
